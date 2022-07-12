0e151f20a61dce7ae02d8e8f53fdc7b8fd1923eb
hkwamods <109180701+hkwamods@users.noreply.github.com>
2022-07-13 03:00:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,18 +54,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['94766012322'] //ur owner number
+global.ownername = "🦄HK BOT" //ur owner name
+global.ytname = "YT: 𝙃𝙆𝙒𝘼 𝙈𝙊𝘿𝙕🍁" //ur yt chanel name
+global.socialm = "GitHub: hkwamodz" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['94766012322']
+global.ownertag = '94766012322' //ur tag number
+global.botname = 'Cheems Bot HK' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UCc3TjjE6MRnS4KXZuO9-oiw" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UCc3TjjE6MRnS4KXZuO9-oiw" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
